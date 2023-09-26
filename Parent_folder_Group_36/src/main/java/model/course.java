34fe32c731df0b59d2c321383ca25d7945e2ccba
Ahmed Shams Saif <ahmedshams3572@gmail.com>
2023-09-27 09:38:43 +1000

    added course and slide classes

--- Parent_folder_Group_36/src/main/java/model/course.java
+++ Parent_folder_Group_36/src/main/java/model/course.java
@@ -2,6 +2,40 @@
 package main.java.model;
 
 public class course {
-    // Contains private attributes and public methods for courses
+    private String courseID;
+    private String title;
+    private String category;
+    private float cost;
+    private String duration;
+    private boolean refundOption;
 
+    // Constructor for Course
+    public course(String courseID, String title, String category, float cost, String duration, boolean refundOption) {
+        this.courseID = courseID;
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.duration = duration;
+        this.refundOption = refundOption;
+    }
+
+    // Method to add a slide
+    public void addSlide(String type) {
+        System.out.println("Slide of type " + type + " added to course " + this.title);
+    }
+
+    // Method to update course title
+    public void updateTitle(String newTitle) {
+        this.title = newTitle;
+    }
+
+    // Method to update course category
+    public void updateCategory(String newCategory) {
+        this.category = newCategory;
+    }
+
+    // Method to update course cost
+    public void updateCost(float newCost) {
+        this.cost = newCost;
+    }
 }
--- Parent_folder_Group_36/src/main/java/model/slide.java
+++ Parent_folder_Group_36/src/main/java/model/slide.java
@@ -2,6 +2,53 @@
 package main.java.model;
 
 public class slide {
-    // Contains private attributes and public methods for slides
+    private String title;
+    private int sequenceNumber;
+    private String type;
 
+    // Constructor for Slide
+    public slide(String title, int sequenceNumber, String type) {
+        this.title = title;
+        this.sequenceNumber = sequenceNumber;
+        this.type = type;
+    }
+
+    // Method to update slide title
+    public void updateTitle(String newTitle) {
+        this.title = newTitle;
+    }
+
+    // Method to update slide sequence number
+    public void updateSequenceNum(int newNumber) {
+        this.sequenceNumber = newNumber;
+    }
 }
+class InformationSlide extends slide {
+    private String type;
+    // Assuming a basic structure for resources database.
+    private String[] resources;
+
+    // Constructor for InformationSlide
+    public InformationSlide(String title, int sequenceNumber, String type, String[] resources) {
+        super(title, sequenceNumber, type);
+        this.resources = resources;
+    }
+
+    // Method to change slide type
+    public void changeType(String newType) {
+        this.type = newType;
+    }
+
+    // Method to add a resource
+    public void addResource(String resource) {
+        // Simple logic to add a resource to the resources list.
+        // In a real-world scenario, you might want a more sophisticated approach.
+        int length = this.resources.length;
+        String[] newResources = new String[length + 1];
+        System.arraycopy(this.resources, 0, newResources, 0, length);
+        newResources[length] = resource;
+        this.resources = newResources;
+    }
+}
+
+
